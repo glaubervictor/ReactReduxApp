6007a332c3c2b099ea2e51ba0c43cf9724cb1b72
Glauber Victor Leite Carvalho <glauber.victor@tron.com.br>
2017-11-16 17:13:14 -0200

    Authentication implements

--- src/reducers/reducers.jsx
+++ src/reducers/reducers.jsx
@@ -2,11 +2,12 @@ import { combineReducers } from 'redux'
 import { reducer as reduxFormReducer } from 'redux-form';
 import { reducer as toastrReducer } from 'react-redux-toastr'
 
-//import AuthReducer from './authReducer'
+import AuthReducer from '../auth/authReducer'
 
 const rootReducer = combineReducers({
     form: reduxFormReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    auth: AuthReducer
 })
 
 export default rootReducer
